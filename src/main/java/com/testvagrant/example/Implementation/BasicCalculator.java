a95a6b403ae99f451125793bed361cd9144136d9
TejasChawda <tejasc@testvagrant.com>
2023-12-08 14:37:15 +0530

    ETE basic calculator refactored

--- src/main/java/com/testvagrant/example/Implementation/BasicCalculator.java
+++ src/main/java/com/testvagrant/example/Implementation/BasicCalculator.java
@@ -1,30 +1,30 @@
 package com.testvagrant.example.Implementation;
 
-public class BasicCalculator extends CalculatorImplementation{
+public class BasicCalculator extends CalculatorImplementation {
 
-        public double add(double num1, double num2) {
-            return num1 + num2;
-        }
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
 
-        public double subtract(double num1, double num2) {
-            return num1 - num2;
-        }
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
 
-        public double multiply(double num1, double num2) {
-            return num1 * num2;
-        }
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
 
-        public double divide(double num1, double num2) {
-            try {
-                if (num2 == 0) {
-                    throw new ArithmeticException("Error! Division by zero is not allowed.");
-                } else {
-                    return num1 / num2;
-                }
-            } catch (ArithmeticException e) {
-                System.out.println(e.getMessage());
-                return Double.NaN;
+    public double divide(double num1, double num2) {
+        try {
+            if (num2 == 0) {
+                throw new ArithmeticException("Error! Division by zero is not allowed.");
+            } else {
+                return num1 / num2;
             }
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            return Double.NaN;
         }
+    }
 
 }
